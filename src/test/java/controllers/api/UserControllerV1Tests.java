ce55457118093c8f620fedc78a8356c838ea789d
Sven Kubiak <sk@svenkubiak.de>
2024-12-26 18:11:01 +0100

    Added test users to unit tests

--- src/test/java/controllers/api/UserControllerV1Tests.java
+++ src/test/java/controllers/api/UserControllerV1Tests.java
@@ -1,10 +1,18 @@
 package controllers.api;
 
+import constants.Const;
 import controllers.TestExtension;
+import io.mangoo.core.Application;
+import io.mangoo.persistence.interfaces.Datastore;
 import io.mangoo.test.http.TestRequest;
 import io.mangoo.test.http.TestResponse;
+import io.mangoo.utils.CodecUtils;
 import io.mangoo.utils.JsonUtils;
 import io.mangoo.utils.MangooUtils;
+import models.Category;
+import models.Item;
+import models.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -15,6 +23,20 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @ExtendWith({TestExtension.class})
 public class UserControllerV1Tests {
+
+    @BeforeAll
+    public static void init() {
+        Datastore datastore = Application.getInstance(Datastore.class);
+        datastore.dropCollection(Category.class);
+        datastore.dropCollection(Item.class);
+        datastore.dropCollection(User.class);
+
+        User user = new User("foo");
+        user.setPassword(CodecUtils.hashArgon2("bar", user.getSalt()));
+        datastore.save(user);
+        datastore.save(new Category(Const.INBOX, user.getUid()));
+        datastore.save(new Category(Const.TRASH, user.getUid()));
+    }
 
     @Test
     void testLogin() {
