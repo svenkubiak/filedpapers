b505d55ec968fc29111177959cf674c110bc72fe
Sven Kubiak <sk@svenkubiak.de>
2025-01-06 16:01:07 +0100

    Cleanup

--- release.sh
+++ release.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-# Exit on any error
 set -e
 
-# Variables
 IMAGE_NAME="filedpapers"
 GHCR_USERNAME="svenkubiak"
 REPO_NAME="filedpapers"
@@ -12,7 +9,6 @@ GHCR_URL="ghcr.io"
 mvn release:clean
 mvn clean verify
 
-# Check if the Maven build was successful
 if [ $? -ne 0 ]; then
   echo "Maven build failed! Exiting..."
   exit 1
@@ -30,34 +26,31 @@ if [ $STATUS -ne 0 ]; then
   echo "Failed to set new version! Exiting..."
   exit 1
 else
-  # Build Docker image
   echo "Building Version Docker image..."
   docker build -t "$IMAGE_NAME:$IMAGE_VERSION" .
 
-    echo "Building Latest Docker image..."
-    docker build -t "$IMAGE_NAME:latest" .
+  echo "Building Latest Docker image..."
+  docker build -t "$IMAGE_NAME:latest" .
 
-  # Tag image for GHCR
   echo "Tagging version as $IMAGE_FULL_PATH..."
   docker tag "$IMAGE_NAME:$IMAGE_VERSION" "$IMAGE_FULL_PATH"
 
   echo "Tagging latest as $IMAGE_LATEST_PATH..."
   docker tag "$IMAGE_NAME:latest" "$IMAGE_LATEST_PATH"
 
-  # Push image to GHCR
   echo "Pushing version to GitHub Container Registry..."
   docker push "$IMAGE_FULL_PATH"
 
   echo "Pushing latest to GitHub Container Registry..."
   docker push "$IMAGE_LATEST_PATH"
 
-  # Verify the push
   if [ $? -eq 0 ]; then
       echo "Image successfully pushed to $IMAGE_FULL_PATH"
       git tag $IMAGE_VERSION
       mvn release:update-versions
       git commit -am "Updated version after release"
       git push --tags origin main
+      echo "Released!"
   else
       echo "Failed to push the image. Exiting..."
       exit 1
@@ -65,4 +58,3 @@ else
 fi
 
 rm pom.xml.versionsBackup
-echo "Released!"
