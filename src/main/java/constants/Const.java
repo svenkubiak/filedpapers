5f20585adc38e2b2d257a2353db6627e83e60cca
Sven Kubiak <sk@svenkubiak.de>
2025-03-18 14:55:22 +0100

    Sonar refactorings

--- src/main/java/constants/Const.java
+++ src/main/java/constants/Const.java
@@ -1,22 +1,22 @@
 package constants;
 
 public class Const {
-    public static final String TOAST_ERROR = "toasterror";
-    public static final String TOAST_SUCCESS = "toastsuccess";
-    public static final String INBOX = "Inbox";
-    public static final String TRASH = "Trash";
-    public static final String USER_UID = "userUid";
-    public static final String CHALLENGE_TOKEN = "challengeToken";
     public static final String ACCESS_TOKEN = "accessToken";
-    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String ACTION = "action";
+    public static final String CHALLENGE_TOKEN = "challengeToken";
+    public static final String DEFAULT_LANGUAGE = "en";
+    public static final String EMAIL_PREFIX = "[Filed Papers]";
+    public static final String INBOX = "Inbox";
+    public static final String MFA_FALLBACK = "mfa-fallback";
     public static final String NONCE = "nonce";
+    public static final String OTP = "otp";
     public static final String PEPPER = "pepper";
     public static final String PLACEHOLDER_IMAGE = "/assets/images/placeholder.png";
-    public static final String OTP = "otp";
-    public static final String MFA_FALLBACK = "mfa-fallback";
-    public static final String ACTION = "action";
-    public static final String EMAIL_PREFIX = "[Filed Papers]";
-    public static final String DEFAULT_LANGUAGE = "en";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String TOAST_ERROR = "toasterror";
+    public static final String TOAST_SUCCESS = "toastsuccess";
+    public static final String TRASH = "Trash";
+    public static final String USER_UID = "userUid";
 
     private Const() {}
 }
