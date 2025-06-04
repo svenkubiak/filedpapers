0f1f6c1d7f1c9da11da40b054a21d9a20e9c6d38
Sven Kubiak <sk@svenkubiak.de>
2025-06-04 14:18:30 +0200

    Sonar refactorings

--- src/main/java/constants/Required.java
+++ src/main/java/constants/Required.java
@@ -3,6 +3,7 @@ package constants;
 public final class Required {
     public static final String ACCESS_TOKEN_SECRET = "accessTokenSecret can not be null";
     public static final String ACTION = "action can not be null";
+    public static final String APPLICATION_URL = "applicationUrl can not be null";
     public static final String AUTHENTICATION_SERVICE = "authenticationService can not be null";
     public static final String CATEGORIES = "categories can not be null";
     public static final String CATEGORY_NAME = "name can not be null";
@@ -11,6 +12,7 @@ public final class Required {
     public static final String CONFIG = "config can not be null";
     public static final String COOKIE_NAME = "cookieName can not be null";
     public static final String COOKIE_SECRET = "cookieSecret can not be null";
+    public static final String DATA = "data can not be null";
     public static final String DATA_SERVICE = "dataService can not be null";
     public static final String DATASTORE = "database can not be null";
     public static final String FROM = "from can not be null";
@@ -18,12 +20,16 @@ public final class Required {
     public static final String ITEMS = "items can not be null";
     public static final String LANGUAGE = "language can not be null";
     public static final String LOGIN_REDIRECT = "loginRedirect can not be null";
+    public static final String MEDIA_SERVICE = "mediaService can not be null";
+    public static final String MEDIA_UID = "mediaUid can not be null";
     public static final String MESSAGES = "messages can not be null";
     public static final String NAME = "name can not be null";
     public static final String NOTIFICATION_SERVICE = "notificationService can not be null";
     public static final String OBJECT = "object can not be null";
     public static final String PASSWORD = "password can not be null";
     public static final String REFRESH_TOKEN_SECRET = "refreshTokenSecret can not be null";
+    public static final String REQUEST = "request can not be null";
+    public static final String RESPONSE = "response can not be null";
     public static final String SECRET = "secret can not be null";
     public static final String TITLE = "title can not be null";
     public static final String TOKEN = "token can not be null";
@@ -31,12 +37,6 @@ public final class Required {
     public static final String USER_UID = "userUid can not be null";
     public static final String USERNAME = "username can not be null";
     public static final String VALUE = "value can not be null";
-    public static final String REQUEST = "request can not be null";
-    public static final String RESPONSE = "response can not be null";
-    public static final String MEDIA_SERVICE = "mediaService can not be null";
-    public static final String MEDIA_UID = "mediaUid can not be null";
-    public static final String DATA = "data can not be null";
-    public static final String APPLICATION_URL = "applicationUrl can not be null";
 
     private Required() {}
 }
