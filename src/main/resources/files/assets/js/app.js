2ccc1aab531f4cee981e1f1093739665d48c7603
Sven Kubiak <sk@svenkubiak.de>
2025-09-12 09:23:13 +0200

    Fixed issue in filtering items

--- src/main/resources/files/assets/js/app.js
+++ src/main/resources/files/assets/js/app.js
@@ -397,9 +397,7 @@ function addBookmark(e) {
 }
 
 function search(e) {
-    if (!e?.target?.value) return;
-
-    const searchTerm = e.target.value.toLowerCase();
+    const searchTerm = e?.target?.value.toLowerCase() || '';
     const cards = $$('.card');
 
     cards.forEach(card => {
@@ -407,7 +405,11 @@ function search(e) {
         const column = card.closest('.column');
 
         if (column) {
-            column.style.display = title.includes(searchTerm) ? '' : 'none';
+            if (!searchTerm) {
+                column.style.display = '';
+            } else {
+                column.style.display = title.includes(searchTerm) ? '' : 'none';
+            }
         }
     });
 }
