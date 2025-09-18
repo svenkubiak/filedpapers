c5048f7bb97d3fd55920af7d6bee6c8e50c05cf3
Sven Kubiak <sk@svenkubiak.de>
2025-09-18 13:15:42 +0200

    Version bumps

--- src/test/java/utils/PreviewTests.java
+++ src/test/java/utils/PreviewTests.java
@@ -10,7 +10,6 @@ import utils.preview.LinkPreview;
 import utils.preview.LinkPreviewFetcher;
 
 import java.io.*;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -139,7 +138,7 @@ public class PreviewTests {
     }
 
     @Test
-    public void testPreview1() throws IOException, URISyntaxException {
+    public void testPreview1() {
         //when
         LinkPreview preview = LinkPreviewFetcher.fetch("https://www.youtube.com/watch?v=jE0Q8zIrXwU", "en");
 
@@ -151,7 +150,7 @@ public class PreviewTests {
     }
 
     @Test
-    public void testPreview2() throws IOException, URISyntaxException {
+    public void testPreview2() {
         //when
         LinkPreview preview = LinkPreviewFetcher.fetch("https://www.mariowiki.com/Mario_Kart_8_Deluxe#Drivers.27_statistics", "en");
 
@@ -163,22 +162,19 @@ public class PreviewTests {
     }
 
     @Test
-    public void testPreview3() throws IOException, URISyntaxException {
+    public void testPreview3() {
         //when
         LinkPreview preview = LinkPreviewFetcher.fetch("https://www.google.de/maps/place/Al-lord+Arabische+S%C3%BC%C3%9Figkeiten/@51.5212403,7.1043508,5547m/data=!3m2!1e3!5s0x47b8e6fe6ebb7f5d:0xb4201f4ae33f6cd2!4m6!3m5!1s0x47b8e7cdd297860d:0xe80458e9d78a93f4!8m2!3d51.5129816!4d7.0943446!16s%2Fg%2F11j33m9_3d?entry=ttu&g_ep=EgoyMDI1MDUyNy4wIKXMDSoASAFQAw%3D%3D", "en");
 
         //then
         assertThat(preview.title(), startsWith("Al-lord Arabische Süßigkeiten · Hansemannstraße 23, 45879 Gelsenkirchen"));
-        assertThat(preview.image(), anyOf(
-                is("https://lh3.googleusercontent.com/p/AF1QipMhSZ68K3lODLzmFL0arjx5fwh0KsizwUjLZGOz=w900-h900-p-k-no"),
-                is("https://lh3.googleusercontent.com/gps-cs-s/AC9h4nompioGOBPa7H9F3Vf1m-AIcx5bzjFnHkC0B6D6nBhDkBgDGAox4iM-UnJus1iLV-mB3BSo4wASsbk4ABZDZ2bUv3UW1cxdGU7YPEi8O7QoTghHRfGauMAm4TsouC0VTghlbHnuqHFgdLY=w900-h900-p-k-no")
-        ));
+        assertThat(preview.image(), startsWith("https://lh3.googleusercontent.com"));
         assertThat(preview.domain(), equalTo("google.de"));
         assertThat(preview.description(), notNullValue());
     }
 
     @Test
-    public void testPreview4() throws IOException, URISyntaxException {
+    public void testPreview4() {
         //when
         LinkPreview preview = LinkPreviewFetcher.fetch("https://github.com/svenkubiak", "en");
 
