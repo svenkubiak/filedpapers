a07c202a0211e29f5bb5e291611920e4f6becb4c
Sven Kubiak <sk@svenkubiak.de>
2026-03-09 09:56:49 +0100

    Build fix

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -2,12 +2,14 @@ package controllers;
 
 import io.mangoo.persistence.interfaces.Datastore;
 import io.mangoo.routing.Response;
+import jakarta.inject.Inject;
 
 import java.util.Objects;
 
 public class ApplicationController {
     private final Datastore datastore;
 
+    @Inject
     public ApplicationController(Datastore datastore) {
         this.datastore = Objects.requireNonNull(datastore, "datastore must not be null");
     }
